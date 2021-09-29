1a82c0b73d73fe0de8a02d18bdc5c22e36750c7a
Anonyknight(Will) <anonyknight@hotmail.com>
2021-09-28 22:58:58 -0700

    Fixed terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,11 @@
-resource "google_compute_instance" "default" {
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
+resource "google_compute_instance" "django_vm" {
   name         = "django"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
+  zone         = var.zone
 
   tags = ["django"]
 
@@ -16,12 +20,36 @@ resource "google_compute_instance" "default" {
 
     access_config {
       // Ephemeral public IP
+      nat_ip = google_compute_address.static.address
     }
   }
 
   metadata = {
-    user-data = file("my_cloud_init.conf")
+    # user-data = file("my_cloud_init.conf")
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
 
   # metadata_startup_script = "echo hi > /test.txt"
+}
+
+resource "null_resource" "code" {
+  depends_on = [google_compute_instance.django_vm]
+  connection {
+    type        = "ssh"
+    host        = google_compute_address.static.address
+    private_key = var.gce_ssh_private_key
+    user        = var.gce_ssh_user
+    port        = "22"
+    agent       = false
+  }
+
+  provisioner "file" {
+    source      = "."
+    destination = "~"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ls -al",
+    ]
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,24 @@ variable "region" {
   type    = string
   default = "us-west1"
 }
+
+
+variable "zone" {
+  type    = string
+  default = "us-west1-b"
+}
+
+variable "gce_ssh_user" {
+  type    = string
+  description = "GCE SSH User"
+}
+
+variable "gce_ssh_pub_key_file" {
+  type    = string
+  description = "GCE SSH User public key"
+}
+
+variable "gce_ssh_private_key" {
+  type    = string
+  description = "GCE SSH User public key"
+}
